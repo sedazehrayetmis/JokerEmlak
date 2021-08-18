31347c322125b5b4ebc2096e1996db43bac7ba46
Your Name <you@example.com>
2021-08-19 01:48:23 +0300

    jokerEmlak

---
+++ JokerEmlak/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JokerEmlak.Models.Entity;
+namespace JokerEmlak.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        dbemlakEntities db = new dbemlakEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLEVLER.ToList();
+            return View(degerler);
+        }
+    }
+}
